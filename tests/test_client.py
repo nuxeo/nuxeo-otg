edf083a599d6a30e2c1091cb5b4e3b165f91d35e
Stefane Fermigier <sf@fermigier.com>
2011-02-17 12:40:53 +0100

    Basic structure for client tests.

--- tests/test_client.py
+++ tests/test_client.py
@@ -7,28 +7,23 @@ PATH = "/tmp"
 USERNAME = PASSWORD = "Administrator"
 
 
-class ClientTest(unittest.TestCase):
+class AbstractClientTest(unittest.TestCase):
+    __test__ = False
 
     def test(self):
         raise "Error"
 
 
-class LocalClientTest(ClientTest):
+class LocalClientTest(AbstractClientTest):
+    __test__ = True
 
     def setUp(self):
         self.client = LocalClient(PATH)
 
 
-class RemoteClientTest(ClientTest):
+class RemoteClientTest(AbstractClientTest):
+    __test__ = True
 
     def setUp(self):
         self.client = RemoteClient(ROOT, USERNAME, PASSWORD, PATH)
 
-
-#def suite():
-#    suite1 = unittest.TestLoader().loadTestsFromTestCase(LocalClientTest)
-#    suite2 = unittest.TestLoader().loadTestsFromTestCase(RemoteClientTest)
-#    return unittest.TestSuite([suite1, suite2])
-
-#if __name__ == '__main__':
-#    unittest.TextTestRunner(verbosity=2).run(suite)
